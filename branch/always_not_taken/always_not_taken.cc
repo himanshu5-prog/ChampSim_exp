b670843e3230caa578abaf4be0ad8442a342af5a
Himanshu Yadav <himanshu07319@gmail.com>
2024-12-01 12:26:15 -0800

    Add always-not-taken branch predictor

---
+++ branch/always_not_taken/always_not_taken.cc
@@ -0,0 +1,10 @@
+#include "msl/fwcounter.h"
+#include "ooo_cpu.h"
+
+void O3_CPU::initialize_branch_predictor() {}
+
+uint8_t O3_CPU::predict_branch(uint64_t ip){
+    return false;
+}
+
+void O3_CPU::last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t taken, uint8_t branch_type){}
